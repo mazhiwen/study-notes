04344921f7f2e8f7ab8061f659d4970e567c8e73
marjoven <81245995@qq.com>
2018-07-17 14:39:47 +0800

    Object高级应用

--- js/advancedObject.js
+++ js/advancedObject.js
@@ -10,11 +10,23 @@ Object.isExtensible(person);
 
 /**************** 密封 seal****************/
 Object.seal(person);
-//密封对象，不可扩展
+//密封对象，不可扩展 ,满足isExtensible
+person. age = 29;//无效
 //已有成员的[[Configurable]]为false,即不可删除属性
+delete person.name;//无效
 //属性值可修改
+//检测是否seal
+Object.isSealed(person);
 
 
+
+/**************** 冻结 freeze****************/
+Object.freeze(person);
+//满足isExtensible 和 isSealed
+//数据属性的[[Writable]]为false ,不可设置值
+person.name='aaa';//无效
+//检测
+Object.isFrozen(person);
 
 
 
