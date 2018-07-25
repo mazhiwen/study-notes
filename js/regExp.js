1d578881017df7fb1adbdb32c55da8c15d8baec8
marjoven <81245995@qq.com>
2018-07-25 14:14:58 +0800

    正则

--- js/regExp.js
+++ js/regExp.js
@@ -2,17 +2,16 @@
 
 
 // n个数字
-
 // /^\d{1,2}$/.test('2');
 
 
-
 // n个非空
-
 // /^\S{1,2}$/.test('2');
-
 
 //匹配多行
 var a=/(.*?)(href="(.*?)"(.*?)[\r\n])*/.exec(`href="aa"
 bbhref=""`);
 console.log(a);
+
+//匹配 /aaa/bbb 中的aaa
+'/aaa/bb'.match(/\/([^/]*)/);
