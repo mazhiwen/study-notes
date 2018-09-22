fad5acf49a61046dafdb341462f7f0ad9ffbb034
marjoven <81245995@qq.com>
2018-09-22 18:27:53 +0800

    up

--- js/String.js
+++ js/String.js
@@ -1,3 +1,8 @@
 /**************** String.slice ****************/
 // 提取一个字符串的一部分，并返回一新的字符串。
 // str.slice(beginSlice[, endSlice])
+
+
+
+/**************** String.replace ****************/
+// str.replace(regexp|substr, newSubStr|function)
